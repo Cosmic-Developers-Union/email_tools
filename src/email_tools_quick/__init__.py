ec560e498a4769a1258dc105c82aa2ed2092e353
“Ge” <cosplox@outlook.com>
2025-08-29 15:36:43 +0800

    feat: 优化异常与导入

--- src/email_tools_quick/__init__.py
+++ src/email_tools_quick/__init__.py
@@ -4,11 +4,25 @@
 """Models Description
 
 """
+from .common import CommonClient
 from .data import EMail
 from .data import MailBox
 from .data import MailBoxMap
+from .error import BaseEmailError
+from .error import FetchEmailError
+from .error import LoginEmailError
 from .mail import IMAP4Client
 from .mail import IMAP4SSLClient
 from .mail import IMAP4_PORT
 from .mail import IMAP4_SSL_PORT
 from .mail import SocketParams
+from .ms import MSClient
+
+__all__ = [
+    "CommonClient",
+    "MSClient",
+
+    "BaseEmailError",
+    "LoginEmailError",
+    "FetchEmailError",
+]
--- src/email_tools_quick/error.py
+++ src/email_tools_quick/error.py
@@ -21,11 +21,3 @@ class FetchEmailError(BaseEmailError):
     """
     Exception raised for errors in fetching emails.
     """
-
-
-class NoSuchMailBoxError(Exception):
-    """Exception raised when the specified mailbox does not exist."""
-
-    def __init__(self, message: str = "Mailbox does not exist."):
-        self.message = message
-        super().__init__(self.message)
